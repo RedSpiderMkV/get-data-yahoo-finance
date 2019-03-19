c9d86effe2223a4e4dfdefa064b9f01c7fe9c709
RedSpiderMkV <redspidermkv@hotmail.com>
2019-03-19 18:07:03 +0000

    working prototype

--- src/getData.py
+++ src/getData.py
@@ -24,6 +24,21 @@ with open('/home/nikeah/yahooFinanceResponse.txt', 'r') as file:
 json_content = json.loads(content[:-1])
 dataStore = json_content['context']['dispatcher']['stores']['StreamDataStore']['quoteData'][symbol]
 
-print(dataStore)
+#print(dataStore)
 
+opening_value = dataStore['regularMarketOpen']['raw']
+high_value = dataStore['regularMarketDayHigh']['raw']
+low_value = dataStore['regularMarketDayLow']['raw']
+volume_value = dataStore['regularMarketVolume']['raw']
 
+change_percentage_value = dataStore['regularMarketChangePercent']['raw']
+change_value = dataStore['regularMarketChange']['raw']
+current_price = dataStore['regularMarketPrice']['raw']
+symbol_value = dataStore['symbol']
+timestamp = dataStore['regularMarketTime']['fmt']
+
+print(symbol)
+print('Price:', current_price)
+print('Change:', change_value)
+print('Change %:', change_percentage_value)
+print('Timestamp:', timestamp)
